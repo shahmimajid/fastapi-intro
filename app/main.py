21260aaba3d9a6d3a8375920e6412c66012ea648
Shahmi Majid <shahmi.majid@gmail.com>
2021-10-24 23:33:24 +0800

    Create / and /page/

--- app/main.py
+++ app/main.py
@@ -1,8 +1,16 @@
 from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
 
 app = FastAPI(title="Fastapi-intro", version="0.0.1")
 
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+async def home():
+    data = {"text": "hi"}
+    return {"data": data}
+
+
+@app.get("/page/{page_name}")
+async def page(page_name: str):
+    data = {"page": page_name}
+    return {"data": data}
