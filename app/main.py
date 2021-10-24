c8d22ca1590f92e008f7fe9023d84bcb5ee693ce
Shahmi Majid <shahmi.majid@gmail.com>
2021-10-24 23:44:18 +0800

    Adding templates

--- app/main.py
+++ app/main.py
@@ -1,16 +1,21 @@
 from fastapi import FastAPI
+from fastapi import templating
 from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+from starlette.requests import Request
 
 app = FastAPI(title="Fastapi-intro", version="0.0.1")
 
-
-@app.get("/")
-async def home():
-    data = {"text": "hi"}
-    return {"data": data}
+templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/page/{page_name}")
-async def page(page_name: str):
+@app.get("/", response_class=HTMLResponse)
+async def home(request: Request):
+    data = {"page": "Home Page"}
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
+
+
+@app.get("/page/{page_name}", response_class=HTMLResponse)
+async def page(request: Request, page_name: str):
     data = {"page": page_name}
-    return {"data": data}
+    return templates.TemplateResponse("page.html", {"request": request, "data": data})
